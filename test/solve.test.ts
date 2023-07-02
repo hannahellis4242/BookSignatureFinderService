2181e122e4dc12226755c0171838eef81b0f73fc
hannahellis4242 <hannahellis4242@gmail.com>
2023-07-02 16:00:32 +0100

    fixing tests

--- test/solve.test.ts
+++ test/solve.test.ts
@@ -1,14 +1,16 @@
+import OutputConfig from "../src/solver/OutputConfig";
 import Problem from "../src/solver/Problem";
 import solve from "../src/solver/solve";
 describe("solve", () => {
+  const config: OutputConfig = { format: "string", includePageCount: true };
   test("null problem", () => {
     const problem: Problem = { allowed: [], range: { min: 0, max: 0 } };
-    const results = solve(problem);
+    const results = solve(problem)(config);
     expect(results).toHaveLength(1);
   });
   test("simple problem", () => {
     const problem: Problem = { allowed: [1], range: { min: 1, max: 6 } };
-    const results = solve(problem);
+    const results = solve(problem)(config);
     expect(results).toHaveLength(1);
   });
   test("test problem", () => {
@@ -16,7 +18,7 @@ describe("solve", () => {
       allowed: [3, 4, 5],
       range: { min: 100, max: 102 },
     };
-    const results = solve(problem);
+    const results = solve(problem)(config);
     expect(results).toHaveLength(8);
   });
 });
